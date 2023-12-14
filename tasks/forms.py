8d4287dac6d3d22347b99d8f33a2f568f12a3dcf
roberto6022 <147462730+roberto6022@users.noreply.github.com>
2023-12-14 13:06:57 -0300

    Delete tasks directory

--- tasks/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-
-class TaskCreationForm(forms.Form):
-    title = forms.CharField(label='Title', max_length=255)
-    content = forms.CharField(label='Content', widget=forms.Textarea())
--- tasks/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class Task(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField()
-
-    def __str__(self):
-        return f'{self.title}, {self.content}'
