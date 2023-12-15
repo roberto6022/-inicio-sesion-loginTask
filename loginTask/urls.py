ac7822055527abf5a625d826979cb3c1a736ba1d
roberto6022 <147462730+roberto6022@users.noreply.github.com>
2023-12-15 20:57:24 -0300

    Update urls.py

--- loginTask/urls.py
+++ loginTask/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tasks/', include('tasks.urls')),
     path('account/', include('account.urls')),
+    path('', lambda request: redirect('account:login'), name='root'),
 ]
